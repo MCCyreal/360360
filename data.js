d1f4848df3604f83806e81b1356d15b4e659117c
MCCyreal <aleksandrovkyrylo@gmail.com>
2025-02-10 19:59:35 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,365 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-szefter-start-360",
+      "name": "Szefter Start 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 2.3983944041783234,
+        "pitch": 0.676859316617044,
+        "fov": 1.3990458089307722
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.143963573521055,
+          "pitch": 0.8399160986260483,
+          "rotation": 0,
+          "target": "1-rka-og-360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.706030857872557,
+          "pitch": -0.16743416744642303,
+          "title": "Kieszenie Pełne Kamienia",
+          "text": "<header style=\"margin-left: 20px; color: rgb(68, 68, 68); font-family: Roboto, sans-serif; font-size: 14px;\"><span style=\"background-color: rgb(255, 255, 255);\">Kiedy pracownia artystki staje się sama w sobie osobliwością, rodzajem niewyjaśnionej żądzy, proces twórczy zaczyna podążać we własnym kierunku. Pokój, okno, roboczy stół przeobrażają się w przestrzeń zbieractwa. Przestrzeń, która sama w sobie staje się problemem twórczym. Rzeczą do odnalezienia.&nbsp;</span></header>"
+        }
+      ]
+    },
+    {
+      "id": "1-rka-og-360",
+      "name": "Ręka Ogół 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.90993203803081,
+          "pitch": 0.16499820297003254,
+          "rotation": 0,
+          "target": "2-0000"
+        },
+        {
+          "yaw": -0.20413745057188137,
+          "pitch": 0.30205137820520633,
+          "rotation": 0,
+          "target": "3-korytarz-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-0000",
+      "name": "0000",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7487018490716544,
+          "pitch": 0.10343035679978385,
+          "rotation": 0,
+          "target": "1-rka-og-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-korytarz-360",
+      "name": "Korytarz 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3198857561173511,
+          "pitch": 0.5576739360789809,
+          "rotation": 0,
+          "target": "4-pracownia-magda"
+        },
+        {
+          "yaw": 0.014108535265281219,
+          "pitch": 0.37941871983934305,
+          "rotation": 0,
+          "target": "7-domek-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pracownia-magda",
+      "name": "Pracownia Magda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.634976251545794,
+          "pitch": 0.30091721953805894,
+          "rotation": 0,
+          "target": "5-stol-pracownia-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-stol-pracownia-360",
+      "name": "Stol Pracownia 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0207332154505426,
+          "pitch": 0.17743096260037738,
+          "rotation": 0,
+          "target": "6-pracownia-krata-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pracownia-krata-360",
+      "name": "Pracownia Krata 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.43764619099401614,
+          "pitch": 0.2328235463484276,
+          "rotation": 0,
+          "target": "3-korytarz-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-domek-360",
+      "name": "Domek 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.891140848887934,
+          "pitch": 0.39853904529367057,
+          "rotation": 0,
+          "target": "8-metal-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-metal-360",
+      "name": "Metal 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
